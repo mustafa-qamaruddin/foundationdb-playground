870fbe46b332fd50b56de14e177018d8c7370d39
Mustafa Qamar-ud-Din <mustafa.mahrous.89@gmail.com>
2024-03-05 09:31:01 +0100

    init foundationdb

---
+++ src/main/java/com/qubits/foundationdb/Main.java
@@ -0,0 +1,31 @@
+package com.qubits.foundationdb;
+
+import com.foundationdb.Database;
+import com.foundationdb.FDB;
+import com.foundationdb.Transaction;
+import com.foundationdb.tuple.Tuple;
+
+public class Main {
+
+  private static final FDB fdb;
+  private static final Database db;
+
+  static {
+    fdb = FDB.selectAPIVersion(300);
+    db = fdb.open();
+  }
+
+  public static void main(String[] args) {
+    // Run an operation on the database
+    db.run((Transaction tr) -> {
+      tr.set(Tuple.from("hello").pack(), Tuple.from("world").pack());
+      return null;
+    });
+    // Get the value of 'hello' from the database
+    String hello = db.run((Transaction tr) -> {
+      byte[] result = tr.get(Tuple.from("hello").pack()).get();
+      return Tuple.fromBytes(result).getString(0);
+    });
+    System.out.println("Hello " + hello);
+  }
+}
